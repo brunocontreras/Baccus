ffeab2c1e91506de34b212dcd4c82926c0720fd1
Bruno Contreras Chicote <bruno.contreras@live.u-tad.com>
2014-11-07 19:43:01 +0100

    Posibilidad de cambiar entre 2 imágenes

--- src/com/utad/baccus/MainActivity.java
+++ src/com/utad/baccus/MainActivity.java
@@ -1,9 +1,9 @@
 package com.utad.baccus;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,24 +13,15 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    
+    public void changeImage(View v){
+    	ImageView wineImage = (ImageView) findViewById(R.id.wine_image);
+    	
+    	if (v.getId() == R.id.btn_Vegabal) {
+    		wineImage.setImageResource(R.drawable.vegabal);
+    	}
+    	else if (v.getId() == R.id.btn_Bembibre) {
+    		wineImage.setImageResource(R.drawable.bembibre);
+    	}
     }
 }
